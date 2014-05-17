02d18b2a76cf69d16999d81e289386be1bbde797
Tyler Peterson <petersontb@familysearch.org>
2014-05-17 16:20:50 -0600

    Start playing with generating queries for a group.
    Make a list of nick-names.
    Make a script for fanning out the nicknames using copies of a parameterized query.
    Ignore the generated queries for git.

---
+++ fan-out-nicknames.js
@@ -0,0 +1,13 @@
+var nicks = require('./ad-hoc-nicknames');
+var tasksQuery = require('./tasks-for-owner');
+var query = [];
+var fs = require('fs');
+
+nicks.forEach(function (nick) {
+  var copy = JSON.parse(JSON.stringify(tasksQuery));
+
+  copy.with["$memberNick"] = nick;
+  query.push(copy);
+});
+
+fs.writeFileSync('./generated/tasks-for-owners-of-ad-hoc-group.json', JSON.stringify(query, 0, 2));
